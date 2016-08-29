03de26880f4cd21113b15cc8fc050bf7065d4aee
stefan.masztalerz <stefan.masztalerz@aoe.com>
2016-08-29 18:54:23 +0200

    [Add] Use composer autoloading - PSR-4

--- Classes/Xclass/DatabaseConnection.php
+++ Classes/Xclass/DatabaseConnection.php
@@ -1,24 +1,5 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2008-2009 Fernando Arconada fernando.arconada at gmail dot com
-*  All rights reserved
-*
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+namespace Aoe\T3p_scalable\Xclass;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -28,7 +9,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 * @author Fernando Arconada fernando.arconada at gmail dot com
 * @author Juergen Kussmann juergen.kussmann at aoe dot com
 */
-class Tx_T3pScalable_Xclass_DatabaseConnection extends \TYPO3\CMS\Core\Database\DatabaseConnection
+class DatabaseConnection extends \TYPO3\CMS\Core\Database\DatabaseConnection
 {
     /**
 	 * @var integer
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,5 +7,5 @@ if (!defined('TYPO3_MODE')) {
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'class.tx_t3pscalable.php';
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\DatabaseConnection'] = array(
-    'className' => 'Tx_T3pScalable_Xclass_DatabaseConnection',
+    'className' => '\\Aoe\\T3p_scalable\\Xclass\\DatabaseConnection',
 );
